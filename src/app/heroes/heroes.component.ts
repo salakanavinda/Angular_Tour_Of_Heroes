aa2fca8b552f5dbaa0fd3bbcaa277f20ce198f80
Salaka Navinda <mmsnavinda@gmail.com>
2019-06-27 09:57:18 +0530

    Update heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HEROS } from '../hero-mock';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  
   hero:Hero={
     id:1,
     name:'salaka'
   };
-
+  heross=HEROS;
   constructor() { }
 
   ngOnInit() {
@@ -19,5 +20,10 @@ export class HeroesComponent implements OnInit {
 
   share(){
     window.alert('sdddd');
+  }
+  selectedHero:Hero;
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
   }
 }
